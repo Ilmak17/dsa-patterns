e527b517f670760679affc1509a2683deb2363b7
Ilyas Makirov <ilmak1704@gmail.com>
2025-05-10 13:35:42 +0500

    hash map implementation

--- src/datastructures/HashMap.java
+++ src/datastructures/HashMap.java
@@ -1,92 +1,50 @@
 package datastructures;
 
 public class HashMap<K, V> {
-    private static final int INITIAL_CAPACITY = 16;
-    private static final double LOAD_FACTOR = 0.75;
+    private static final int CAPACITY = 16;
 
-    private Entry<K, V>[] table;
-    private int size;
+    private final Bucket[] table;
 
     public HashMap() {
-        table = new Entry[INITIAL_CAPACITY];
-        size = 0;
+        this.table = new Bucket[CAPACITY];
     }
 
     public void put(K key, V value) {
-        if ((double) size / table.length > LOAD_FACTOR) {
-            resize();
-        }
-
         int index = findSlot(key);
-        if (table[index] == null || table[index].isDeleted) {
-            table[index] = new Entry<>(key, value);
-            size++;
-        } else {
-            table[index].value = value;
-        }
+        table[index] = new Bucket<>(key, value);
     }
 
     public V get(K key) {
         int index = findSlot(key);
-        if (table[index] == null || table[index].isDeleted) {
-            return null;
+        Bucket<K, V> bucket = table[index];
+        if (bucket != null && bucket.key.equals(key)) {
+            return bucket.value;
         }
-        return table[index].value;
-    }
-
-    public V remove(K key) {
-        int index = findSlot(key);
-        if (table[index] == null || table[index].isDeleted) {
-            return null;
-        }
-
-        V value = table[index].value;
-        table[index].isDeleted = true;
-        size--;
-        return value;
+        return null;
     }
 
     public boolean containsKey(K key) {
         int index = findSlot(key);
-        return table[index] != null && !table[index].isDeleted;
+        return table[index] != null && table[index].key.equals(key);
     }
-
-    public int size() {
-        return size;
-    }
-
 
     private int findSlot(K key) {
-        int index = Math.abs(key.hashCode()) % table.length;
+        int index = Math.abs(key.hashCode()) % CAPACITY;
 
-        while (table[index] != null && (!table[index].key.equals(key) || table[index].isDeleted)) {
-            index = (index + 1) % table.length;
+        while (table[index] != null && !table[index].key.equals(key)) {
+            index = (index + 1) % CAPACITY;
         }
 
         return index;
     }
 
-    private void resize() {
-        Entry<K, V>[] oldTable = table;
-        table = new Entry[oldTable.length * 2];
-        size = 0;
-
-        for (Entry<K, V> entry : oldTable) {
-            if (entry != null && !entry.isDeleted) {
-                put(entry.key, entry.value);
-            }
-        }
-    }
-
-    private static class Entry<K, V> {
+    private static class Bucket<K, V> {
         K key;
         V value;
-        boolean isDeleted;
 
-        Entry(K key, V value) {
+        Bucket(K key, V value) {
             this.key = key;
             this.value = value;
-            this.isDeleted = false;
         }
     }
 }
