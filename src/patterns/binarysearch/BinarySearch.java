94be77e40bdfed75de73d2a2de129b43e5a6d6b4
Ilyas Makirov <ilmak1704@gmail.com>
2025-05-17 22:50:11 +0500

    binary search in rotated arr implementation

--- src/patterns/binarysearch/BinarySearch.java
+++ src/patterns/binarysearch/BinarySearch.java
@@ -51,4 +51,31 @@ public class BinarySearch {
         return left;
     }
 
+    public int searchInRotatedArray(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == k) return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= k && k < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < k && nums[right] >= k) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
 }
