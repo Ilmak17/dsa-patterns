65ee5c193db9686b5138b9055af808eecc4c57af
Ilyas Makirov <ilmak1704@gmail.com>
2025-05-17 22:49:31 +0500

    binary search lower/upper bound implementation

--- src/patterns/binarysearch/BinarySearch.java
+++ src/patterns/binarysearch/BinarySearch.java
@@ -17,4 +17,38 @@ public class BinarySearch {
 
         return -1;
     }
+
+    public int lowerBound(int[] nums, int x) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public int upperBound(int[] nums, int x) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
 }
