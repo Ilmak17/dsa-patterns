e73a5ef8e7db761f4019fa3d0cc6391e92d8b455
Ilyas Makirov <ilmak1704@gmail.com>
2025-05-17 22:52:43 +0500

    binary search peak element implementation

--- src/patterns/binarysearch/BinarySearch.java
+++ src/patterns/binarysearch/BinarySearch.java
@@ -147,4 +147,21 @@ public class BinarySearch {
         return false;
     }
 
+    public int findPeakElement(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
 }
