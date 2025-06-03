065cea331f07804fa018a6e710d6ab7cb2cb79eb
Ilyas Makirov <ilmak1704@gmail.com>
2025-06-03 18:45:54 +0500

    Graph union find implementation

--- src/patterns/graph/UnionFind.java
+++ src/patterns/graph/UnionFind.java
@@ -1,4 +1,39 @@
 package patterns.graph;
 
 public class UnionFind {
+
+    private int[] parent;
+    private int[] rank;
+
+    public UnionFind(int n) {
+        this.parent = new int[n];
+        this.rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+        }
+    }
 }
