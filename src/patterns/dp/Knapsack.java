f1e5ae8c555c0bf7d16878a665b2364ac06aba7b
Ilyas Makirov <ilmak1704@gmail.com>
2025-06-14 23:54:28 +0500

    DP implementation

--- src/patterns/dp/Knapsack.java
+++ src/patterns/dp/Knapsack.java
@@ -20,4 +20,24 @@ public class Knapsack {
 
         return dp[W];
     }
+
+    public int knapsackUnbounded(int[] wt, int[] val, int n, int W) {
+        int[] dp = new int[W + 1];
+
+        for (int c = wt[0]; c <= W; c++) {
+            dp[c] = val[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = W; j >= 0; j--) {
+                int taken = 0;
+                if (j >= wt[i]) {
+                    taken = val[i] + dp[j - wt[i]];
+                }
+                dp[j] = Math.max(dp[j], taken);
+            }
+        }
+
+        return dp[W];
+    }
 }
