6aa0b143c053c22f8e29a4a7937bd2fb5649e0e3
Ilyas Makirov <ilmak1704@gmail.com>
2025-06-15 23:01:57 +0500

    DP implementation

--- src/patterns/dp/LIS.java
+++ src/patterns/dp/LIS.java
@@ -1,4 +1,25 @@
 package patterns.dp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LIS {
+    public int lis(int[] nums) {
+        int n = nums.length;
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > temp.get(temp.size() - 1)) {
+                temp.add(nums[i]);
+            } else {
+                int ind = Collections.binarySearch(temp, nums[i]);
+                if (ind < 0) ind = -(ind + 1);
+                temp.set(ind, nums[i]);
+            }
+        }
+
+        return temp.size();
+    }
 }
