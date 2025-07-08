861b3edc6088c936c8047e66877f55c9f2f4940a
Ilyas Makirov <ilmak1704@gmail.com>
2025-07-08 22:31:17 +0500

    DP implementation

--- src/patterns/dp/OnGrids.java
+++ src/patterns/dp/OnGrids.java
@@ -1,5 +1,26 @@
 package patterns.dp;
 
 public class OnGrids {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int[] tmp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    tmp[j] = 1;
+                    continue;
+                }
+                int up = (i > 0) ? prev[j] : 0;
+                int left = (j > 0) ? tmp[j - 1] : 0;
+
+                tmp[j] = up + left;
+            }
+            prev = tmp;
+        }
+
+        return prev[n - 1];
+    }
+
 
 }
