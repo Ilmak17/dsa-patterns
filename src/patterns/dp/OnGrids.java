d49705c07e3e8afb7ff6997ee68cc9a56229dc9a
Ilyas Makirov <ilmak1704@gmail.com>
2025-07-09 14:15:13 +0500

    DP implementation

--- src/patterns/dp/OnGrids.java
+++ src/patterns/dp/OnGrids.java
@@ -63,5 +63,28 @@ public class OnGrids {
         return mini;
     }
 
+    public int minTriangleSum(int[][] triangle) {
+        int n = triangle.length;
+        int[] front = new int[n];
+
+        int[] cur = new int[n];
+
+        for (int j = 0; j < n; j++) {
+            front[j] = triangle[n - 1][j];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                int down = triangle[i][j] + front[j];
+                int diagonal = triangle[i][j] + front[j + 1];
+
+                cur[j] = Math.min(down, diagonal);
+            }
+            front = cur.clone();
+        }
+
+        return front[0];
+    }
+
 
 }
