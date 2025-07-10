b64738fde6e014c5c01d2828746d6c861d7aa888
Ilyas Makirov <ilmak1704@gmail.com>
2025-07-10 21:45:26 +0500

    DP implementation

--- src/patterns/dp/OnStocks.java
+++ src/patterns/dp/OnStocks.java
@@ -2,4 +2,18 @@ package patterns.dp;
 
 public class OnStocks {
 
+    public int stockBuySell(int[] arr, int n) {
+        int min = arr[0];
+        int profit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            profit = Math.max(profit, arr[i] - min);
+            min = Math.min(arr[i], min);
+        }
+
+        return profit;
+    }
+
+
+
 }
